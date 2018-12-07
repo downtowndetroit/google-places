5dd60480f8801de5271124acab790c565d545cbd
tianxie <13590388977@163.com>
2018-12-07 11:22:35 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ apikey='YOUR API KEY'
 
 #[bounding]
 bounding = {
-  south:42.328166
-  west:-83.056673
-  north:42.332751
-  east:-83.053417
+  'south':42.328166
+  'west':-83.056673
+  'north':42.332751
+  'east':-83.053417
 }
 #[searchingParameter]
 
@@ -16,8 +16,8 @@ bounding = {
 #;Because of the limited response by Google API(60 for each requests), you want
 #;to make sure the output record doesn't exceed that limit
 searchingParameter = {
-  searchingRadius:300
-  type:'store'
+  'searchingRadius':300
+  'type':'store'
 }
 
 #specify the type of the place you want to scrape
